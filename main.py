729b1be9aacbc1f3d542e6ef4ee1a0f591b888ff
atuiyish <tuyi95.at@gmail.com>
2025-06-07 20:13:41 +0200

    fix(élément non nécessaire dans le main)

--- main.py
+++
@@ -1,8 +0,0 @@
-from package.package_class_Aime import *
-from package.package_class_Nehemie import *
-from package.GUI import *
-
-
-if __name__ == "__main__":
-    # Lancement de la boucle principale de l'interface graphique
-    fenetre_login.mainloop()
