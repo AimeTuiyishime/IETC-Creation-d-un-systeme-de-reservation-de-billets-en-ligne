2c74a1473af4e4d164553ee01676a2f5f33f00aa
atuiyish <tuyi95.at@gmail.com>
2025-06-22 20:03:29 +0200

    refactor(package,tests unitaire): Centralise les tests et nettoie les fichiers redondants

--- package/categorie_de_siege.py
+++
@@ -1,8 +0,0 @@
-class CategorieDeSiege:
-    def __init__(self, idCategorie, nomCategorie, prix):
-        self.idCategorie = idCategorie
-        self.nomCategorie = nomCategorie
-        self.prix = prix
-    
-    def __str__(self):
-        return f"[{self.idCategorie}] {self.nomCategorie} - {self.prix} "
